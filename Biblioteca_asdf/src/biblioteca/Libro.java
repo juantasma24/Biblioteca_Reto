fce515f9451b9facdf21f80ef26378b391e85501
alvarourien <24dw.alvaro.urien@arangoya.net>
2025-01-17 13:43:02 +0100

    volvemos a añadir los atributos -Alvaro

--- Biblioteca_asdf/src/biblioteca/Libro.java
+++ Biblioteca_asdf/src/biblioteca/Libro.java
@@ -1,4 +1,11 @@
 package biblioteca;
 
 public class Libro {
+  String titulo;
+    String autor;
+    String id;
+    int añoPublicacion;
+    int copiasDisponibles;
+    String[] reservas;
+    int numReservas;
 }
