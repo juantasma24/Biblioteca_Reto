9debf01e824248bcab9e782ca72fbc46c9bef6ba
alvarourien <24dw.alvaro.urien@arangoya.net>
2025-01-17 13:47:24 +0100

    añadimos el constructor -Alvaro

--- Biblioteca_asdf/src/biblioteca/Libro.java
+++ Biblioteca_asdf/src/biblioteca/Libro.java
@@ -8,4 +8,19 @@ public class Libro {
     int copiasDisponibles;
     String[] reservas;
     int numReservas;
+
+ Libro(String titulo, String autor, String id, int añoPublicacion, int copiasDisponibles) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.id = id;
+        this.añoPublicacion = añoPublicacion;
+        this.copiasDisponibles = copiasDisponibles;
+        this.reservas = new String[10]; 
+        this.numReservas = 0;
+    }
+
+
+
+
+  
 }
