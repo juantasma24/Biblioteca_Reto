55f9efe8fab32ca5bed579ccea2237d56e259654
juantasma24 <24dw.juan.chancafe@arangoya.net>
2025-01-17 13:47:24 +0100

    Se agregaron constructores -Juandiego-

--- Biblioteca_asdf/src/biblioteca/Usuario.java
+++ Biblioteca_asdf/src/biblioteca/Usuario.java
@@ -1,2 +1,19 @@
 package biblioteca;
 
+public class Usuario
+{
+    String nombre;
+    String id;
+    String rol;
+    String[] librosPrestados;
+    int numLibrosPrestados;
+
+    Usuario(String nombre, String id, String rol)
+    {
+        this.nombre = nombre;
+        this.id = id;
+        this.rol = rol;
+        this.librosPrestados = new String[5];
+        this.numLibrosPrestados = 0;
+    }
+
