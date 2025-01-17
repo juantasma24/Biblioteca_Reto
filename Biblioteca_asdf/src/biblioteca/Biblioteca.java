310de0c992391455278a943b02f4aebb5cc06cd7
ibai022 <24dw.ibai.bilbao@arangoya.net>
2025-01-17 13:59:27 +0100

    Atributos - Ibai
    Escribir atributos dentro de la clase biblioteca

--- Biblioteca_asdf/src/biblioteca/Biblioteca.java
+++ Biblioteca_asdf/src/biblioteca/Biblioteca.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 public class Biblioteca 
 {
-  
+  //Atributos
+	Usuario[] usuarios;
+  Libro[] libros;
+  Prestamo[] prestamos;
+  Reserva[] reservas;
+  int numUsuarios;
+  int numLibros;
+  int numPrestamos;
+  int numReservas;
 }
 
