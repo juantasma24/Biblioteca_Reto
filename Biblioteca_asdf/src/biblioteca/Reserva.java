6b46b9198034961294e6dac31edb6d38bd9a4748
alvarourien <24dw.alvaro.urien@arangoya.net>
2025-01-17 13:14:50 +0100

    añadimos los metodos a reserva -Alvaro

--- Biblioteca_asdf/src/biblioteca/Reserva.java
+++ Biblioteca_asdf/src/biblioteca/Reserva.java
@@ -13,5 +13,40 @@ public class Reserva {
         this.usuarioId = usuarioId;
         this.libroId = libroId;
         this.fechaReserva = new Date(); // Se establece la fecha actual como fecha de la reserva
+ 
+    void registrarReserva(String usuarioId) {
+        if (numReservas < reservas.length) {
+            reservas[numReservas] = usuarioId;
+            numReservas++;
+        } else {
+            System.out.println("No se pueden agregar más reservas. Capacidad máxima alcanzada.");
+        }
+    }
+
+    void liberarReserva() {
+        if (numReservas > 0) {
+            System.out.println("Reserva liberada: " + reservas[0]);
+            for (int i = 1; i < numReservas; i++) {
+                reservas[i - 1] = reservas[i];
+            }
+            reservas[numReservas - 1] = null;
+            numReservas--;
+        } else {
+            System.out.println("No hay reservas para liberar.");
+        }
+    }
+
+    void mostrarReservas() {
+        System.out.print("Reservas actuales: ");
+        if (numReservas == 0) {
+            System.out.println("No hay reservas.");
+        } else {
+            for (int i = 0; i < numReservas; i++) {
+                System.out.print(reservas[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
     }
 }
