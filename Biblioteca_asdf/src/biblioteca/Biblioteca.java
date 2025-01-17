e5fb3f17ff9066a183698cd8c3363f78a4a4feec
ibai022 <24dw.ibai.bilbao@arangoya.net>
2025-01-17 14:02:08 +0100

    Constructor
    Implementación del constructor dentro de la clase bliblioteca

--- Biblioteca_asdf/src/biblioteca/Biblioteca.java
+++ Biblioteca_asdf/src/biblioteca/Biblioteca.java
@@ -13,5 +13,19 @@ public class Biblioteca
   int numLibros;
   int numPrestamos;
   int numReservas;
+
+  //Constructor
+  public Biblioteca() 
+  {
+        this.usuarios = new Usuario[100];
+        this.libros = new Libro[100];
+        this.prestamos = new Prestamo[100];
+        this.reservas = new Reserva[100];
+        this.numUsuarios = 0;
+        this.numLibros = 0;
+        this.numPrestamos = 0;
+        this.numReservas = 0;
+    }
+  
 }
 
