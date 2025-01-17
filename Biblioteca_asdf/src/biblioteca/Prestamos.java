fa9b1b27c14cc3554f0043749c8f1620f62bc1a6
tonidaw <24dw.anthony.chimborazo@arangoya.net>
2025-01-17 13:51:41 +0100

    Explicamos la funciones de cada atributo

--- Biblioteca_asdf/src/biblioteca/Prestamos.java
+++ Biblioteca_asdf/src/biblioteca/Prestamos.java
@@ -3,10 +3,10 @@ package prueba;
 import java.util.Date;
 
 public class Prestamo {
-    String idPrestamo;
-    String libroId;
-    String usuarioId;
-    Date fechaPrestamo;
-    Date fechaDevolucion;
+    String idPrestamo;// identificador unico de prestamo
+    String libroId; // identificador del lbro prestado
+    String usuarioId; // identificador del usuario que coge prestado el libro
+    Date fechaPrestamo; //fecha cuando se realiza el prestamo
+    Date fechaDevolucion; // fecha de su devolucion, hasta que no lo devuelva devolvera null.
 
 
